edf79159400445f88a5e5203628cd082dcf81bba
Caleb_Wolf <cwolf1424@gmail.com>
2023-10-30 00:29:48 -0500

    Progress

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,68 @@
 //Create Startup Function
 function init() {
-    let data = [{
-        x:[],
-        y:[] 
-    }];
-    //Plotly.newPlot("plot", data);
+
 }
 
 //Create Funtion to Fill Dropdown
-function fillDropdown(ids) {
+function fillDropdown(idsList) {
     let dropDown=d3.select("#selDataset");
-    ids.forEach(id => {
+    idsList.forEach(id => {
         dropDown.append('option').text(id).property('value', id);
     })
 }
 
-//Create Function to Fill Data Bar
-function fillDemographics(subjects) {
+function optionChanged(){
     let currentID = d3.select("#selDataset").property("value")
-    console.log (currentID)
-    //subjects.forEach(subject => {
-    //    if (subject.id == currentID) {
- 
-    //    } 
-    //})
+    console.log(`Current ID: ${currentID}`)
+
+    let pannelBody = d3.select("#sample-metadata")
+    
+    //Fill Demographics
+    metadata.forEach(subject => { 
+        if (subject.id == currentID) {
+            
+            curID = subject.id;
+            curEth = subject.ethnicity;
+            curGend = subject.gender;
+            curAge = subject.age;
+            curLoc = subject.location;
+            curBBType = subject.bbtype;
+            curWFreq = subject.wfreq;
+            
+            console.log(subject);
+            console.log(`id: ${curID}`);
+            console.log(`ethnicity: ${curEth}`);
+            console.log(`gender: ${curGend}`);
+            console.log(`age: ${curAge}`);
+            console.log(`location: ${curLoc}`);
+            console.log(`bbtype: ${curBBType}`);
+            console.log(`wfreq: ${curWFreq}`);
+        }
+    })
+
+    //Create Bar Chart
+    samples.forEach(sample => {
+        if (sample.id == currentID) {
+            let otuTrace = {
+                 x: sample.sample_values,
+                 y: sample.otu_ids,
+                 name: sample.otu_labels,
+                type: 'bar'
+                };
+            
+                let otuLayout = {
+                    title: "Prevalence of Operational Taxonomic Units"
+                }
+
+            Plotly.newPlot("plot", [otuTrace], {layout});     
+        }
+    })
+}
+
+//Create Function to Fill Data Bar
+function fillDemographics(subjectsDict) {
+    let pannelBody = d3.select("#sample-metadata");
+
 }
 
 //Create Function to Do Bar Graph
@@ -47,13 +86,12 @@ const samplesDataUrl = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz
 d3.json(samplesDataUrl).then (function(data){
     testSubjIDs=data.names;
     metadata=data.metadata;
+    samples=data.samples
 
-    fillDemographics()
+    optionChanged()
     fillDropdown(testSubjIDs);
-
-
-    console.log (data);
+    console.log ("Complete");
 });
 
-//init();
+init();
 
