b796ca675d30e276b6ee1ccd5c582bd5a82c1ffc
Caleb_Wolf <cwolf1424@gmail.com>
2023-10-29 22:26:55 -0500

    Changes

--- static/js/app.js
+++ static/js/app.js
@@ -8,15 +8,34 @@ function init() {
 }
 
 //Create Funtion to Fill Dropdown
-function fillDropdown(list) {
-    let dropDown=d3.select("#selDataset")
-    list.forEach(id => {
-        dropDown.append('option').text(id).property('value', id)
+function fillDropdown(ids) {
+    let dropDown=d3.select("#selDataset");
+    ids.forEach(id => {
+        dropDown.append('option').text(id).property('value', id);
     })
 }
 
-//Create Function to Do Bar Graph
+//Create Function to Fill Data Bar
+function fillDemographics(subjects) {
+    let currentID = d3.select("#selDataset").property("value")
+    console.log (currentID)
+    //subjects.forEach(subject => {
+    //    if (subject.id == currentID) {
+ 
+    //    } 
+    //})
+}
 
+//Create Function to Do Bar Graph
+function barGraph(xvals,yvals) {
+    let trace = {
+        x: xvals,
+        y: yvals,
+        type: 'bar'
+    }
+
+    Plotly.newPlot("plot", data);
+}
 
 //Create Funciton to Do Bubble Chart
 
@@ -26,9 +45,14 @@ function fillDropdown(list) {
 const samplesDataUrl = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 d3.json(samplesDataUrl).then (function(data){
-    testSubjIDs=data.names
-    fillDropdown(testSubjIDs)
-    console.log (data)
+    testSubjIDs=data.names;
+    metadata=data.metadata;
+
+    fillDemographics()
+    fillDropdown(testSubjIDs);
+
+
+    console.log (data);
 });
 
 //init();
