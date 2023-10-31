796ee69dbdab380ae33ac4771ff7fe41cf87025e
Caleb_Wolf <cwolf1424@gmail.com>
2023-10-30 23:17:27 -0500

    Working

--- static/js/app.js
+++ static/js/app.js
@@ -5,18 +5,26 @@ function init() {
 
 //Create Funtion to Fill Dropdown
 function fillDropdown(idsList) {
-    let dropDown=d3.select("#selDataset");
+    let dropDown = d3.select("#selDataset");
+    
     idsList.forEach(id => {
         dropDown.append('option').text(id).property('value', id);
     })
 }
 
 function optionChanged(){
+    
     let currentID = d3.select("#selDataset").property("value")
-    console.log(`Current ID: ${currentID}`)
-
     let pannelBody = d3.select("#sample-metadata")
     
+    //Clear Demographics Info Pannel
+    pannelBody.html("")
+    let unorderedList = pannelBody.append("ul")
+
+    //Grab Metadata for Current Subject
+    //let currentSubject = metadata.filter(function(subject) {if (subject.id = currentID) {return subject}})
+    
+
     //Fill Demographics
     metadata.forEach(subject => { 
         if (subject.id == currentID) {
@@ -29,41 +37,73 @@ function optionChanged(){
             curBBType = subject.bbtype;
             curWFreq = subject.wfreq;
             
-            console.log(subject);
-            console.log(`id: ${curID}`);
-            console.log(`ethnicity: ${curEth}`);
-            console.log(`gender: ${curGend}`);
-            console.log(`age: ${curAge}`);
-            console.log(`location: ${curLoc}`);
-            console.log(`bbtype: ${curBBType}`);
-            console.log(`wfreq: ${curWFreq}`);
+            unorderedList.append("p").text(`ID: ${curID}`)
+            unorderedList.append("p").text(`Ethnicity: ${curEth}`)
+            unorderedList.append("p").text(`Gender: ${curGend}`)
+            unorderedList.append("p").text(`Age: ${curAge}`)
+            unorderedList.append("p").text(`Location: ${curLoc}`)
+            unorderedList.append("p").text(`BBType: ${curBBType}`)
+            unorderedList.append("p").text(`WFreq: ${curWFreq}`)
         }
     })
 
     //Create Bar Chart
     samples.forEach(sample => {
         if (sample.id == currentID) {
-            let otuTrace = {
-                 x: sample.sample_values,
-                 y: sample.otu_ids,
+            let sampleOTUs= sample.otu_ids.map(function(otuID) {
+                newOTUId=`OTU ${otuID}`;
+                return newOTUId
+            })
+        
+            let otuTrace = [{
+                 x: sampleOTUs,
+                 y: sample.sample_values,
                  name: sample.otu_labels,
                 type: 'bar'
-                };
+                }];
             
                 let otuLayout = {
-                    title: "Prevalence of Operational Taxonomic Units"
+                    title: "Prevalence of Operational Taxonomic Units",
                 }
+            Plotly.newPlot("bar", otuTrace, otuLayout);     
+        }
+    })
 
-            Plotly.newPlot("plot", [otuTrace], {layout});     
+    //Create Bubble Chart
+    samples.forEach(sample => {
+        if (sample.id == currentID) {
+            
+            let sampleOTUs= sample.otu_ids.map(function(otuID) {
+                newOTUId=`OTU ${otuID}`;
+                return newOTUId
+            })
+        
+            let otuTrace2 = [{
+                 x: sample.otu_ids,
+                 y: sample.sample_values,
+                 text: sample.otu_ids,
+                 mode: 'markers',
+                 marker: {
+                    color: ["red", "green", "blue", "purple", "grey", "black"],
+                    size: sample.sample_values,
+                    sizeref: 2
+                 }
+                }];
+            
+                let otuLayout2 = {
+                    title: "Prevalence of Operational Taxonomic Units",
+                    xaxis: {
+                        title:"OTU ID"
+                    }
+                };
+
+            Plotly.newPlot("bubble", otuTrace2, otuLayout2);     
         }
     })
 }
 
-//Create Function to Fill Data Bar
-function fillDemographics(subjectsDict) {
-    let pannelBody = d3.select("#sample-metadata");
+    
 
-}
 
 //Create Function to Do Bar Graph
 function barGraph(xvals,yvals) {
@@ -76,17 +116,17 @@ function barGraph(xvals,yvals) {
     Plotly.newPlot("plot", data);
 }
 
-//Create Funciton to Do Bubble Chart
+
 
 
 //Use D3 to read in samples.json
 
 const samplesDataUrl = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-d3.json(samplesDataUrl).then (function(data){
-    testSubjIDs=data.names;
-    metadata=data.metadata;
-    samples=data.samples
+d3.json(samplesDataUrl).then(function(data){
+    testSubjIDs = data.names;
+    metadata = data.metadata;
+    samples =data.samples;
 
     optionChanged()
     fillDropdown(testSubjIDs);
