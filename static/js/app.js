68a53f15c97ae376a6c3a1687936daf3f9f01f4e
Caleb_Wolf <cwolf1424@gmail.com>
2023-10-31 18:43:45 -0500

    Filter working

--- static/js/app.js
+++ static/js/app.js
@@ -22,30 +22,46 @@ function optionChanged(){
     let unorderedList = pannelBody.append("ul")
 
     //Grab Metadata for Current Subject
-    //let currentSubject = metadata.filter(function(subject) {if (subject.id = currentID) {return subject}})
+    let currentSubject = metadata.filter(function(item) {if (item.id == currentID) {return item}})
+    console.log (currentSubject)
     
+    let curID = currentSubject[0].id;
+    let curEth = currentSubject[0].ethnicity;
+    let curGend = currentSubject[0].gender;
+    let curAge = currentSubject[0].age;
+    let curLoc = currentSubject[0].location;
+    let curBBType = currentSubject[0].bbtype;
+    let curWFreq = currentSubject[0].wfreq;
+
+    unorderedList.append("p").text(`ID: ${curID}`)
+    unorderedList.append("p").text(`Ethnicity: ${curEth}`)
+    unorderedList.append("p").text(`Gender: ${curGend}`)
+    unorderedList.append("p").text(`Age: ${curAge}`)
+    unorderedList.append("p").text(`Location: ${curLoc}`)
+    unorderedList.append("p").text(`BBType: ${curBBType}`)
+    unorderedList.append("p").text(`WFreq: ${curWFreq}`)
 
     //Fill Demographics
-    metadata.forEach(subject => { 
-        if (subject.id == currentID) {
+    //metadata.forEach(subject => { 
+    //    if (subject.id == currentID) {
             
-            curID = subject.id;
-            curEth = subject.ethnicity;
-            curGend = subject.gender;
-            curAge = subject.age;
-            curLoc = subject.location;
-            curBBType = subject.bbtype;
-            curWFreq = subject.wfreq;
+    //        curID = subject.id;
+    //        curEth = subject.ethnicity;
+    //       curGend = subject.gender;
+    //        curAge = subject.age;
+    //        curLoc = subject.location;
+    //        curBBType = subject.bbtype;
+    //        curWFreq = subject.wfreq;
             
-            unorderedList.append("p").text(`ID: ${curID}`)
-            unorderedList.append("p").text(`Ethnicity: ${curEth}`)
-            unorderedList.append("p").text(`Gender: ${curGend}`)
-            unorderedList.append("p").text(`Age: ${curAge}`)
-            unorderedList.append("p").text(`Location: ${curLoc}`)
-            unorderedList.append("p").text(`BBType: ${curBBType}`)
-            unorderedList.append("p").text(`WFreq: ${curWFreq}`)
-        }
-    })
+    //        unorderedList.append("p").text(`ID: ${curID}`)
+    //        unorderedList.append("p").text(`Ethnicity: ${curEth}`)
+    //        unorderedList.append("p").text(`Gender: ${curGend}`)
+    //        unorderedList.append("p").text(`Age: ${curAge}`)
+    //        unorderedList.append("p").text(`Location: ${curLoc}`)
+    //        unorderedList.append("p").text(`BBType: ${curBBType}`)
+    //        unorderedList.append("p").text(`WFreq: ${curWFreq}`)
+    //    }
+    //})
 
     //Create Bar Chart
     samples.forEach(sample => {
@@ -126,11 +142,9 @@ const samplesDataUrl = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz
 d3.json(samplesDataUrl).then(function(data){
     testSubjIDs = data.names;
     metadata = data.metadata;
-    samples =data.samples;
-
-    optionChanged()
+    samples = data.samples;
     fillDropdown(testSubjIDs);
-    console.log ("Complete");
+    optionChanged();
 });
 
 init();
